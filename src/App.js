8db879df83634808e999b041a6471fdd784b8fbb
Jafar99 <jafaralihajaro@gmail.com>
2023-02-23 12:48:53 +0530

    commit6

--- src/App.js
+++ src/App.js
@@ -6,20 +6,21 @@ import {Row,Col,Space} from 'antd';
 function App() {
   return (
     <div className="App">
-   
+        <div>
         <Row>
         <Col span={3} className="kra">
           <img  src={kra} alt="" className='img'></img>
         </Col>
-        <Col span={21}  className="marq" >
-            <marquee  >
+        <Col span={20}  className="marq" >
+            {/* <marquee  > */}
             <div className="marquee" ><span className='text'>Gold 24KT - RS.5677.20 Per Gm</span></div>
             <div  className="marquee1"><span className='text'>Gold 24KT - RS.5283.70 Per Gm</span></div>
             <div  className="marquee"><span className='text'>Gold 24KT - RS.4496.80 Per Gm</span></div>
             <div  className="marquee1"><spa className='text'>Pune Sliver - Rs.6680 per Gm</spa></div>
-            </marquee>
+            {/* </marquee> */}
         </Col>
         </Row>
+        </div>
      
     </div>
   );
